a140963c2c7f376a3a5b5d2be504c1e2e1d0be6b
Fredrik Tillman <fredrikt@activesolution.se>
2021-09-04 08:31:40 +0200

    Fixes db bug

--- Migrations/20210904062808_somechange.Designer.cs
+++ Migrations/20210904062808_somechange.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using flytt2021.Data.Database;
 
 namespace flytt2021.Migrations
 {
     [DbContext(typeof(FlyttDbContext))]
-    partial class FlyttDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210904062808_somechange")]
+    partial class somechange
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20210904062808_somechange.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace flytt2021.Migrations
+{
+    public partial class somechange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BoxOwners_Move_MoveId",
+                table: "BoxOwners");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Packers_Move_MoveId",
+                table: "Packers");
+
+            migrationBuilder.RenameColumn(
+                name: "PackerId",
+                table: "Packers",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "BoxOwnerId",
+                table: "BoxOwners",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MoveId",
+                table: "Packers",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MoveId",
+                table: "BoxOwners",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BoxOwners_Move_MoveId",
+                table: "BoxOwners",
+                column: "MoveId",
+                principalTable: "Move",
+                principalColumn: "MoveId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Packers_Move_MoveId",
+                table: "Packers",
+                column: "MoveId",
+                principalTable: "Move",
+                principalColumn: "MoveId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BoxOwners_Move_MoveId",
+                table: "BoxOwners");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Packers_Move_MoveId",
+                table: "Packers");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Packers",
+                newName: "PackerId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "BoxOwners",
+                newName: "BoxOwnerId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MoveId",
+                table: "Packers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MoveId",
+                table: "BoxOwners",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BoxOwners_Move_MoveId",
+                table: "BoxOwners",
+                column: "MoveId",
+                principalTable: "Move",
+                principalColumn: "MoveId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Packers_Move_MoveId",
+                table: "Packers",
+                column: "MoveId",
+                principalTable: "Move",
+                principalColumn: "MoveId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
